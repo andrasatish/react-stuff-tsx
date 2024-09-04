a17ef6fbd804f49a84a1f5f3dff0a975c02f6433
Satish <43448483+andrasatish@users.noreply.github.com>
2024-09-04 17:31:01 +0530

    props from parent to child

--- src/components/01.Props/01_Basic/child.tsx
+++ src/components/01.Props/01_Basic/child.tsx
@@ -1,9 +1,70 @@
-const ChildComponent = (props:any) => {
-    const { nameText } = props;
+import { InterestedStates, TouristDetail } from "./parent";
+import './child.css';
+
+const ChildComponent = (props: any) => {
+    const { nameText, empId, adultStatus, userList, touristData } = props;
     return (
         <>
             <h2>Child Component</h2>
-            { nameText && <p>Name : { nameText }</p>}
+            {nameText && <p>Name : {nameText}</p>}
+            {empId && <p>Employee ID : {empId}</p>}
+            {adultStatus && <p>Adult Status : {JSON.stringify(adultStatus)}</p>}
+            {userList && (
+                <p>{userList.name}</p>
+            )}
+            {
+                touristData && (
+                    <div className="tourist-container">
+                        {
+                            touristData.map((tourist: TouristDetail) => (
+                                <>
+                                    <div className="tourist-flex-item">
+                                        <span>Name </span>
+                                        <span>:</span>
+                                        <span>{tourist.name}</span>
+                                    </div>
+                                    <div className="tourist-flex-item">
+                                        <span>Age </span>
+                                        <span>:</span>
+                                        <span>{tourist.age}</span>
+                                    </div>
+                                    <div className="tourist-flex-item">
+                                        <span>Location </span>
+                                        <span>:</span>
+                                        <span>{tourist.location}</span>
+                                    </div>
+                                    <div className="tourist-flex-item">
+                                        <span>Address Street</span>
+                                        <span>:</span>
+                                        <span>{tourist.address.street}</span>
+                                    </div>
+                                    <div className="tourist-flex-item">
+                                        <span>Address State</span>
+                                        <span>:</span>
+                                        <span>{tourist.address.state}</span>
+                                    </div>
+                                    <div className="tourist-flex-item">
+                                        <span>Address Country</span>
+                                        <span>:</span>
+                                        <span>{tourist.address.country}</span>
+                                    </div>
+                                    {
+                                        tourist.interestedStates.map((eachState: InterestedStates) => (
+                                            <div className="tourist-flex-item">
+                                                <span>Interested State</span>
+                                                <span>:</span>
+                                                <span>{eachState.state}</span>
+                                            </div>
+                                        ))
+                                    }
+                                </>
+                            )
+                            )
+                        }
+
+                    </div>
+                )
+            }
         </>
     )
 }
--- src/components/01.Props/01_Basic/parent.tsx
+++ src/components/01.Props/01_Basic/parent.tsx
@@ -1,12 +1,61 @@
 import { useState } from "react";
 import ChildComponent from "./child";
 
+interface UserDetail {
+    name:string;
+    age: number; 
+    location:string; 
+    designation:string
+}
+
+export interface TouristDetail {
+    name: string;
+    age: number | string;
+    location: string;
+    address: TouristAddress,
+    interestedStates : InterestedStates[] //array of objects
+}
+
+interface TouristAddress {
+    street: string;
+    state: string;
+    country: string;
+    countryID: number | string;
+}
+
+export interface InterestedStates {
+    state: string;
+}
+
+const touristInitialData = [
+    { name: 'Sudheer', age: 28, location: 'Hyderabad', 
+        address: {
+            street: 'KPHB',
+            state: 'TS',
+            country: 'India',
+            countryID: '9999'
+        },
+        interestedStates:  [ {state:'TG'},{state:'MH'},{state:'DL'} ]
+    }
+];
+
+
 const ParentProps = () => {
     const [name, setName] = useState<string>('Satish');
+    const [empoyeeId, setEmployeeId] = useState<number>(10001);
+    const [isAdult, setIsAdult] = useState<boolean>(true);
+    const [userData, setUserData] = useState<UserDetail | null>({name:'Sudheer',designation:'Software',age:28,location:'Hyderabad'});
+    const [touristData, setTouristData] = useState<TouristDetail[]>(touristInitialData);
+
     return (
         <>
             <h1>Parent Props</h1>
-            <ChildComponent nameText={name}/>
+            <ChildComponent 
+                nameText={name} 
+                empId={empoyeeId} 
+                adultStatus={isAdult} 
+                userList={userData} 
+                touristData={touristData}/>
         </>
     )
 }
